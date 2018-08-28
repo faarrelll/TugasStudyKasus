9e23618611c5d51c28ef81a0ec399d42341810fc
ASUS <ASUS@DESKTOP-OM3DURN>
2018-08-28 12:23:09 +0700

    commit keseluruhan jobsheet3

--- src/tugas/Uji.java
+++ src/tugas/Uji.java
@@ -5,12 +5,15 @@
  */
 package tugas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ASUS
  */
 public class Uji {
     public static void main(String[] args) {
+        Scanner masukan = new Scanner(System.in);
         Transaksi uji = new Transaksi(0,0,"","",0,"","","",0);
         System.out.println("Kasir = Admin 1");
         System.out.println("Tagnggal = 28 Agustus 2018");
@@ -18,5 +21,25 @@ public class Uji {
         System.out.println();
         System.out.println();
         uji.outputtt();
+        int bil;
+        System.out.println();
+        System.out.println();
+        
+        do {   
+            System.out.print("\n'1' Beli Lagi \n'2' Selesai\n\nSilahkan Pilih : ");
+            bil = masukan.nextInt();
+            System.out.println();
+            System.out.println();
+            switch (bil)
+        {
+            case 1 :  uji.outputtt();
+                 break;
+            case 2 :  System.out.println("Terima Kasih...");
+              break;
+        }
+            
+        } while(bil !=2);
+        
+        
     }
 }
